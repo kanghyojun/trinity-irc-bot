8ccfd24aa7d36c46706742ec21d83b210994abc0
admire <admire9@gmail.com>
2011-12-28 04:08:51 +0900

    Geronimo

--- bot.js
+++ bot.js
@@ -1,9 +1,7 @@
-var fs = require('fs');
-var irc = require('irc');
-var trinity = require('./trinity');
+var fs = require('fs'),
+    irc = require('irc'),
 
 fs.readFile('./config.json', function (err, data) {
-  if (err) throw err;
-  trinity(new irc.Client(null, null, JSON.parse(data)));
+  var config = JSON.parse(data);
 });
 
