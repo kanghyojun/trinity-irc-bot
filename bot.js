56aee20fea8261414f8fa9db91fa2ba1731358b1
admire <admire9@gmail.com>
2011-12-29 01:38:36 +0900

    Now trinity bot talk back when he called

--- bot.js
+++ bot.js
@@ -1,10 +1,16 @@
 var fs = require('fs');
 
 var irc = require('irc');
-t 
+
 var config = (function() {
   var data = fs.readFileSync('./config.json');
   return JSON.parse(data);
 })();
 
 var bot = new irc.Client(config.server, config.nick, config.options);
+
+bot.addListener('message', function(from, to, message) {
+  if(/^trinity\:.*$/.test(message)) {
+    this.say(to, 'yes sir');
+  }
+});
