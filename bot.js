fd7c4a9dc7e9222d10c0c3fbc6e788dd7238bab6
admire <admire9@gmail.com>
2011-12-29 01:53:41 +0900

    Implement script functions.

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 
 var irc = require('irc');
 
+var env = require('./env');
+
 var config = (function() {
   var data = fs.readFileSync('./config.json');
   return JSON.parse(data);
@@ -10,7 +12,32 @@ var config = (function() {
 var bot = new irc.Client(config.server, config.nick, config.options);
 
 bot.addListener('message', function(from, to, message) {
-  if(/^trinity\:.*$/.test(message)) {
-    this.say(to, 'yes sir');
+  if(/^trinity\:.*$/.test(message)) { 
+    var token = message.split(" ");
+    if(env.command.indexOf(token[1]) != -1 ) {
+      if(token[1] == 'script') {
+        this.say(to, this.script(token));
+      }
+
+    } else {
+      this.say(to, '니가 뭔소리하는지 모르겠당께.');
+    }
   }
 });
+
+bot.script = function(token) {
+
+  token.shift();
+  token.shift();
+
+  var script = token.join(" "),
+      result = null;
+
+  try {
+    result = eval(script);
+  } catch(err) {
+    result = "Error occured ::> " + err;
+  }
+
+  return result;
+}
